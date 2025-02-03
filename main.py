3d99053e17a286828e792581ce112778baa8dc8a
accessaidan <aidan.hedges.001@gmail.com>
2025-02-03 15:56:39 +0000

    i dont fucking know

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ def main_menu_sub():
     frm_pokedex_menu.pack_forget()
     frm_show_teams_menu.pack_forget()
     frm_login_menu.pack_forget()
-    frm_new_account.pack_forget()
-    frm_new_account_button.pack_forget()
-    frm_login_button.pack_forget()
+    frm_register.forget()
+
     frm_main_menu.pack()
 #subroutine to show the pokedex 
 def show_dex_sub():
@@ -29,23 +28,16 @@ def show_teams_sub():
 def login_menu_sub():
     frm_main_menu.pack_forget()
     frm_login_menu.pack()
-    frm_new_account_button.pack()
-    frm_login_button.pack()
+
 #subroutine to make new account
-def new_account_sub():
-    print("called")
-    frm_login_button.pack_forget()
-    frm_new_account.pack()
+def new_user_sub():
+    print("1")
+    btn_login.forget()
+    print("2")
+    frm_register.pack()
+    print("3")
 #subroutine to login to account
-def login_sub():
-    frm_new_account_button.pack_forget()
-def check_new_user():
-    username = username_input.get()
-    password = password_input.get()
-    confirm_password = confirm_password_input.get()
-    if username in user_data:
-        alert_window = tk.Tk()
-        alert_label = tk.Label(text= "That username has already been used")
+
 
 
 
@@ -124,49 +116,27 @@ frm_login_menu.pack()
 back = tk.Button(frm_login_menu, text="Back", fg="Black",height=6,width=12, command=main_menu_sub)
 back.place(x= 5, y=5 )
 
+#Register button
+btn_register= tk.Button(frm_login_menu, text="Register", fg="Black",height=6,width=12, command=new_user_sub)
+btn_register.place(x=265, y=265)
+
+##register button functions
+##Shows username, password, confirm password
+##makes frame
+frm_register = tk.Frame(window, width=910, height=910)
+frm_register.pack()
+
+lbl_new_username = tk.Label(frm_register, textvariable= 'Enter username', height=3, width=24)
+lbl_new_username.place(x=265, y=395)
 
 
-
-#make a new account button ###
-frm_new_account_button = tk.Frame(window, width=910, height=910)
-frm_new_account_button.pack()
-
-btn_new_account = tk.Button(frm_new_account_button, text="Register", fg="black",height=6,width=12,command= new_account_sub)
-btn_new_account.place(x= 265, y=265)
-
-
-
-
-
-#frame with inputs for new account
-frm_new_account = tk.Frame(window, width=910, height=910)
-frm_new_account.pack()
-#entry box for new username, password and confirm password
-username_label = tk.Label( frm_new_account, text="Username: ", fg="black", width=12)
-username_label.place(x= 265,y= 395)
-username_input = tk.Entry( frm_new_account, textvariable="Username", fg="black", width=24)
-username_input.place(x= 395, y= 395) 
-password_label = tk.Label(frm_new_account, text="Password: ", fg="black", width=12)
-password_label.place(x= 265, y= 525)
-password_input = tk.Entry(frm_new_account, textvariable="password", fg="black", width=24)
-password_input.place(x= 395, y= 525)
-confirm_password_label = tk.Label(frm_new_account, text="confirm Password: ", fg="black", width=12)
-confirm_password_label.place(x= 265, y= 655)
-confirm_password_input = tk.Entry(frm_new_account, textvariable="confirm password", fg="black", width=24)
-confirm_password_input.place(x=395, y=655)
-
-submit_button = tk.Button(frm_new_account, text="submit", command= check_new_user )
-submit_button.place(x=395, y=785)
+#Login button
+btn_login= tk.Button(frm_login_menu, text="Login", fg="Black",height=6,width=12,)
+btn_login.place(x=525, y=265)
 
 
 
 
-#login button ###
-frm_login_button = tk.Frame(window, width=910, height=910)
-frm_login_button.pack()
-
-btn_login = tk.Button(frm_login_button, text="Login", fg="black", height=6, width=12 ,command=login_sub)
-btn_login.place(x= 525, y=265)
 
 user_data = pd.read_csv("user_data.csv")
 
